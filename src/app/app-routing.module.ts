02b2752e329e885eaee484e6466f07bbade71b86
Danielmagox <daniel1305972@gmail.com>
2020-12-17 10:59:06 +0000

    ADD listar component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'AngularSpringCRUD';
+  title = "AngularSpringCRUD";
+
+  constructor(private router: Router) {}
+
+  //navega a la ruta de app-routing.module.ts
+  Listar() {
+    this.router.navigate(["listar"]);
+  }
+  Nuevo() {
+    this.router.navigate(["add"]);
+  }
 }
