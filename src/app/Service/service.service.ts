549f86bf77e4012dd37afe3a24a100e2de6f54d9
Danielmagox <daniel1305972@gmail.com>
2020-12-17 15:13:38 +0000

    ADD tables and SPRING connection

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Persona } from "../Modelo/Persona";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ServiceService {
+  Url = "http://localhost:8080/AngularSpringCRUD/personas";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPersonas() {
+    return this.http.get<Persona[]>(this.Url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ListarComponent } from './Persona/listar/listar.component';
-import { EditComponent } from './Persona/edit/edit.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { AddComponent } from './Persona/add/add.component';
+import { AppComponent } from "./app.component";
+import { ListarComponent } from "./Persona/listar/listar.component";
+import { EditComponent } from "./Persona/edit/edit.component";
+import { AppRoutingModule } from ".//app-routing.module";
+import { AddComponent } from "./Persona/add/add.component";
+import { FormsModule } from "@angular/forms";
+import { ServiceService } from "../app/Service/service.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListarComponent,
-    EditComponent,
-    AddComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ListarComponent, EditComponent, AddComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [ServiceService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
