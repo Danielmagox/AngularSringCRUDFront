d8c50496d466561353d33556f0c4aa3c957de6cf
Danielmagox <daniel1305972@gmail.com>
2020-12-18 09:43:52 +0000

    ADD guardar in add component and service

--- src/app/Persona/add/add.component.ts
+++ src/app/Persona/add/add.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Persona } from "src/app/Modelo/Persona";
+import { ServiceService } from "src/app/Service/service.service";
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  selector: "app-add",
+  templateUrl: "./add.component.html",
+  styleUrls: ["./add.component.css"],
 })
 export class AddComponent implements OnInit {
+  constructor(private router: Router, private service: ServiceService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  Guardar(persona: Persona) {
+    this.service.createPersona(persona).subscribe((data) => {
+      alert("Se Agrego con Exito...!!!");
+      this.router.navigate(["listar"]);
+    });
   }
-
 }
--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -14,4 +14,8 @@ export class ServiceService {
   getPersonas(): Observable<Persona[]> {
     return this.http.get<Persona[]>(this.Url);
   }
+
+  createPersona(persona: Persona) {
+    return this.http.post<Persona>(this.Url, persona);
+  }
 }
